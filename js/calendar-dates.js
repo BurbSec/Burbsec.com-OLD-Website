fa7db333322b2d3a968440301f49b4f70b187cd4
Steven McGrath <steve@chigeek.com>
2015-01-27 10:16:42 -0600

    Site Redesign

---
+++ js/calendar-dates.js
@@ -0,0 +1,62 @@
+calendar_events = {
+	// Burbsec Meetups
+	// -------------------------------------------------------------------------
+	// Prime meetups also have the Cigars badge associated with them, so be sure
+	// to add the badge information.
+	"2015-01-01": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-01-08": {"url": "#north"},
+	"2015-01-15": {"url": "#west"},
+	"2015-02-05": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-02-12": {"url": "#north"},
+	"2015-02-19": {"url": "#west"},
+	"2015-02-26": {"url": "#east"},
+	"2015-03-05": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-03-12": {"url": "#north"},
+	"2015-03-19": {"url": "#west"},
+	"2015-03-26": {"url": "#east"},
+	"2015-04-02": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-04-09": {"url": "#north"},
+	"2015-04-16": {"url": "#west"},
+	"2015-04-23": {"url": "#east"},
+	"2015-05-07": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-05-14": {"url": "#north"},
+	"2015-05-21": {"url": "#west"},
+	"2015-05-28": {"url": "#east"},
+	"2015-06-04": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-06-11": {"url": "#north"},
+	"2015-06-18": {"url": "#west"},
+	"2015-06-25": {"url": "#east"},
+	"2015-07-02": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-07-09": {"url": "#north"},
+	"2015-07-16": {"url": "#west"},
+	"2015-07-23": {"url": "#east"},
+	"2015-08-06": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-08-13": {"url": "#north"},
+	"2015-08-20": {"url": "#west"},
+	"2015-08-27": {"url": "#east"},
+	"2015-09-03": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-09-10": {"url": "#north"},
+	"2015-09-17": {"url": "#west"},
+	"2015-09-24": {"url": "#east"},
+	"2015-10-01": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-10-08": {"url": "#north"},
+	"2015-10-15": {"url": "#west"},
+	"2015-10-22": {"url": "#east"},
+	"2015-11-05": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-11-12": {"url": "#north"},
+	"2015-11-19": {"url": "#west"},
+	"2015-11-26": {"url": "#east"},
+	"2015-12-03": {"url": "#prime", "badgeClass": "badge-cigars", "number": "C"},
+	"2015-12-10": {"url": "#north"},
+	"2015-12-17": {"url": "#west"},
+	"2015-12-24": {"url": "#east"},
+
+	// Burbsec gaming nights
+	// Example: "2015-12-10": {"url": "#gaming" "class": "gaming"},
+
+	// Burbsec racing nights
+	// Example: "2015-12-10": {"url": "#racing" "class": "racing"},
+
+	// Burbsec cigar standalone nights
+	// Example: "2015-12-10": {"url": "#cigars" "class": "cigars"},
+}
