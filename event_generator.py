817409fbb09f01361fd4b04f1f5f36182b806000
Johnny Xmas <antonio.m@kasada.io>
2019-01-04 09:40:37 -0600

    Date and Meetup Info Modifications
    Updated date script to no longer add CigarSec dates since it is no longer a regular meetup.
    
    Modified CigarSec info
    
    Manually modified "weird" dates for East and West in 2019
    
    Uncommented and modified BurbSecCon info since sponsors are asking for a website

--- event_generator.py
+++ event_generator.py
@@ -3,7 +3,7 @@ import json
 
 events = dict()
 
-for year in range(2014, 2031):
+for year in range(2019, 2031):
     for month in range(1,13):
         week_counter = 0
         for day in range(1,32):
@@ -16,11 +16,7 @@ for year in range(2014, 2031):
                     week_counter += 1
                     ftoday = '{}-{:02d}-{:02d}'.format(today.year, today.month, today.day)
                     if week_counter == 1:
-                        events[ftoday] = {
-                            'url': '#prime',
-                            'badgeClass': 'badge-cigars',
-                            'number': 'C'
-                        }
+                        events[ftoday] = {'url': '#prime'}
                     elif week_counter == 2:
                         events[ftoday] = {'url': '#north'}
                     elif week_counter == 3:
